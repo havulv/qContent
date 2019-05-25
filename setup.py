6bd716eedc07e6557cdda9b6d09d87752a3b7b52
j <johnandersen185@gmail.com>
2019-05-24 22:38:22 -0400

    Updating requirements based on new code.

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-from setuptools import setup
+#! /usr/bin/env python3.6
+
+from setuptools import setup, find_packages
 
 setup(
-    name='asynccont',
+    name='qc',
     version='0.1.0',
-    packages=['asynccont']
-)
-
+    packages=find_packages(exclude=['*.tests', '*.tests.*',
+                                    'tests.*', 'tests']),
+    package_dir={'qc': 'qc'},
+    url='https://github.com/jandersen7/qContent',
+    author="John Andersen",
+    author_email="johnandersen185@gmail.com",
+    description="Check for content changes on your favorite sites.",
+    install_requires=['aiohttp>=1.2.0', 'async-timeout>=1.1.0'],
+    scripts=['qc/scripts/qc'])
