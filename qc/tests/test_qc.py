7f5cde351476278d82d5084030007aa58d41b144
j <johnandersen185@gmail.com>
2019-05-24 22:44:49 -0400

    Adding baseline testing files -- empty rn.

---
+++ qc/tests/test_qc.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3.7
+
+from .. import qContent as qc
+import unittest
+import nose
+
+
+class InputTestsqC(unittest.TestCase):
+
+    def test_case(self):
+        assert True
+
+
+class UnitTestsqC(unittest.TestCase):
+
+    def test_case(self):
+        assert True
+
+
+class FunctionalTestsqC(unittest.TestCase):
+
+    def test_case(self):
+        assert True
